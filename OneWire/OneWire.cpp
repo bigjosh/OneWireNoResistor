92ec78ebf04428b59610d5e16f86b16016e4cccf
bigjosh <github@junk.josh.com>
2014-06-23 00:53:01 -0400

    Simplified write_bytes()
    Since idle bus state is always left either driven high or pulled-up
    after each write() depending on value of "power", no need to set it
    again at end of write_bytes() since it calls write().

--- OneWire/OneWire.cpp
+++ OneWire/OneWire.cpp
@@ -290,14 +290,11 @@ void OneWire::write(uint8_t v, uint8_t power /* = 0 */) {
 }
 
 void OneWire::write_bytes(const uint8_t *buf, uint16_t count, bool power /* = 0 */) {
-  for (uint16_t i = 0 ; i < count ; i++)
-    write(buf[i]);
-  if (!power) {
-    noInterrupts();
-    DIRECT_MODE_INPUT(baseReg, bitmask);
-    interrupts();
+  for (uint16_t i = 0 ; i < count ; i++) {
+    write(buf[i] , power );
   }
 }
+
 //
 // Read a byte
 //
